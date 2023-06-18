36e90aa72d1f4ba9541414deb0e95b9e7f8491f6
victoralbucorde <126472403+victoralbucorde@users.noreply.github.com>
2023-06-18 12:45:06 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -6,5 +6,14 @@ package q4
 //Você deve implementar uma solução com complexidade de tempo linear e sem memória extra.
 
 func SingleNumber(nums []int) int {
-	return 0
+	numero := 0
+	for i:=0;i<len(nums)-1;i++{
+		for p:=i+1;p<len(nums);p++{
+			if nums[i] == nums[p]{
+				break
+			}
+		}
+		numero = i
+	}
+	return numero
 }

c9c6578254a9b8483df852c6913135f44b960628
RodrigoHLemos <127106444+RodrigoHLemos@users.noreply.github.com>
2023-06-18 00:16:36 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -5,5 +5,37 @@ package q2
 //Se não houver um prefixo comum, retorne uma string vazia "".
 
 func LongestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := strs[0]
+
+	for i := 1; i < len(strs); i++ {
+		for !startsWith(strs[i], prefix) {
+			prefix = prefix[:len(prefix)-1]
+			if prefix == "" {
+				return ""
+			}
+		}
+	}
+
+	return prefix
+}
+
+func startsWith(str, prefix string) bool {
+	if len(str) < len(prefix) {
+		return false
+	}
+
+	for i := 0; i < len(prefix); i++ {
+		if str[i] != prefix[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
 	return ""
 }

85bbd5ed9ae9cb4272efd7725a29996faad7da4e
victoralbucorde <126472403+victoralbucorde@users.noreply.github.com>
2023-06-18 13:54:58 -0300

    Update q5.go

--- q5/q5.go
+++ q5/q5.go
@@ -14,8 +14,8 @@ func IsPalindrome(s string) bool {
 	s = strings.ReplaceAll(s, " ", "")
 	s = strings.ToLower(s)
 
-	for i := 0; i <= len(s)/2; i++ {
-		for p := len(s); p >= len(s)/2; p-- {
+	for i := 0; i < len(s)/2; i++ {
+		for p := len(s); p > len(s)/2; p-- {
 			if s[p] != s[i] {
 				return false
 			}

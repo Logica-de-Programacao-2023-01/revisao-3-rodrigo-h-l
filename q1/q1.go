e7cd56824b9d6499dea8f84309893e70f374e4d4
RodrigoHLemos <127106444+RodrigoHLemos@users.noreply.github.com>
2023-06-18 00:12:28 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -8,5 +8,15 @@ package q1
 //Você pode retornar a resposta em qualquer ordem.
 
 func TwoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+
+	for i, num := range nums {
+		complement := target - num
+		if index, ok := numMap[complement]; ok {
+			return []int{index, i}
+		}
+
+		numMap[num] = i
+		}
 	return nil
 }

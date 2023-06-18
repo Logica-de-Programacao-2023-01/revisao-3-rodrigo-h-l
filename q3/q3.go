1523dec40c0e6156115e00dde92015650c24ed96
victoralbucorde <126472403+victoralbucorde@users.noreply.github.com>
2023-06-18 11:52:52 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -5,5 +5,8 @@ package q3
 //A cada vez, você pode subir 1 ou 2 degraus. De quantas maneiras distintas você pode subir até o topo?
 
 func ClimbStairs(n int) int {
-	return 0
+	fibonacci := []int{1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584}
+
+	
+	return fibonacci[n-1]
 }
